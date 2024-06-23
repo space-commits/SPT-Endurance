692332142dbd16fa871a03b620d155fdff42569b
space-commits <57108281+space-commits@users.noreply.github.com>
2024-06-23 22:08:34 +0100

    Revert "removed logging"
    This reverts commit 409bcc2876ab9e85db965bdf49ad311adc7f5891.

--- Plugin.cs
+++ Plugin.cs
@@ -15,6 +15,7 @@ namespace Endurance
 
             new EnduranceSprintActionPatch().Enable();
             new EnduranceMovementActionPatch().Enable();
+            Logger.LogInfo($"Plugin Fontaine's Always Level Endurance (ALE) is loaded!");
         }
     }
 }
